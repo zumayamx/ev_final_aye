0462bd436224d3860a91ca3be7e6b519ed8a296f
zumayamx <josezumayamx@MacBook-Air-de-Jose-3.local>
2023-11-12 12:06:49 -0600

    add class graph

---
+++ Graph.hpp
@@ -0,0 +1,147 @@
+//
+//  Graph.hpp
+//  Graph
+//
+//  Created by Vicente Cubells on 10/11/20.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "Vertex.hpp"
+
+template <class V, class E>
+class Graph {
+
+    std::vector < Vertex<V, E> * > nodes;
+
+public:
+
+    Graph() {}
+    ~Graph();
+
+    void addVertex(V & );
+    void addVertex(Vertex<V,E> * );
+    void addEdge(Vertex<V,E> *, Vertex<V,E> *, const E & );
+    void removeEdge(Vertex<V,E> *, Vertex<V,E> *, const E & );
+
+    Vertex<V, E> * search(const V & );
+    Vertex<V, E> * search(const Vertex<V,E> *);
+
+    size_t size() const;
+    template <class Vn, class En>
+    friend std::ostream & operator <<(std::ostream &, const Graph<Vn,En> &);
+};
+
+template<class V, class E>
+size_t Graph<V, E>::size() const {
+    return nodes.size();
+}
+
+template <class V, class E>
+Graph<V,E>::~Graph()
+{
+    for (auto v: nodes) {
+        delete v;
+    }
+
+    nodes.clear();
+}
+
+template <class V, class E>
+void Graph<V,E>::addVertex(V & value )
+{
+    auto vertex = new Vertex<V, E>(value);
+
+    nodes.push_back(vertex);
+}
+
+template <class V, class E>
+void Graph<V,E>::addVertex(Vertex<V,E> * vertex )
+{
+    nodes.push_back(vertex);
+}
+
+template <class V, class E>
+void Graph<V,E>::addEdge(Vertex<V,E> * source, Vertex<V,E> * target, const E & value)
+{
+    /* Buscar vertex origen */
+    auto node = find(nodes.begin(), nodes.end(), source);
+
+    /* Crear un edge y adicionarlo al vertex */
+    Edge<V, E> * edge = new Edge<V,E>(value, target);
+
+    (*node)->addEdge(edge);
+}
+
+template <class V, class E>
+void Graph<V,E>::removeEdge(Vertex<V,E> * source, Vertex<V,E> * target, const E & value )
+{
+    /* Buscar vertex origen */
+    auto node = find(nodes.begin(), nodes.end(), source);
+
+    Vertex<V,E> * vertex = (*node);
+
+    auto * edges = vertex->getEdges();
+
+    Edge<V,E> * to_remove = nullptr;
+
+    for (auto e : *edges) {
+        if (e->getInfo() == value && e->getTarget() == target) {
+            to_remove = e;
+            break;
+        }
+    }
+
+    if (to_remove) {
+        vertex->removeEdge(to_remove);
+    }
+
+}
+
+template <class V, class E>
+Vertex<V, E> * Graph<V,E>::search(const V & value )
+{
+    /* Crear un nodo */
+    Vertex<V,E> * vertex = new Vertex<V, E>(value);
+
+    /* Buscar vertex */
+    auto node = search(vertex);
+
+    /* Elimiinar el nodo temporal */
+    delete vertex;
+
+    /* Regresar el nodo encontrado */
+    return node;
+}
+
+template <class V, class E>
+Vertex<V, E> * Graph<V,E>::search(const Vertex<V,E> * value )
+{
+  for (auto n : nodes){
+    
+    if (* n == * value){
+      return n;
+    }
+    
+  }
+  return nullptr;
+}
+
+template <class V, class E>
+std::ostream & operator <<(std::ostream & os, const Graph<V,E> & graph)
+{
+    os << "--- Visualizar grafo:  ---" << std::endl;
+
+    for (auto v :  graph.nodes) {
+        os << *v;
+    }
+
+    return os;
+}
+
+
+#endif /* Graph_hpp */
---
+++ Vertex.hpp
@@ -0,0 +1,100 @@
+//
+//  Vertex.hpp
+//  actividad4-2
+//
+//  Created by José Manuel García Zumaya on 08/11/23.
+//
+
+#ifndef Vertex_hpp
+#define Vertex_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "Edge.hpp"
+
+template <class V, class E>
+class Vertex {
+    V info;
+    std::vector< Edge<V, E> * > edges;
+
+public:
+    Vertex() {}
+    Vertex(V _info) : info(_info) {}
+    ~Vertex();
+
+    V getInfo() const;
+    void setInfo(const V &);
+
+    std::vector< Edge<V,E> * > * getEdges();
+
+    void addEdge(Edge<V,E> *);
+    void removeEdge(Edge<V,E> *);
+
+    bool operator ==(const Vertex<V,E> &);
+
+    template <class Vn, class En>
+    friend std::ostream & operator <<(std::ostream &, const Vertex<Vn, En> &);
+
+};
+
+template <class V, class E>
+Vertex<V,E>::~Vertex()
+{
+    for (auto e : edges) {
+        delete e;
+    }
+
+    edges.clear();
+}
+
+template <class V, class E>
+V Vertex<V,E>::getInfo() const
+{
+    return info;
+}
+
+template <class V, class E>
+void Vertex<V,E>::setInfo(const V & value)
+{
+    info = value;
+}
+
+template <class V, class E>
+std::vector< Edge<V,E> * > * Vertex<V, E>::getEdges()
+{
+    return &edges;
+}
+
+template <class V, class E>
+void Vertex<V,E>::addEdge(Edge<V,E> * edge)
+{
+    edges.push_back(edge);
+}
+
+template <class V, class E>
+void Vertex<V,E>::removeEdge(Edge<V,E> * edge)
+{
+    auto to_delete = find(edges.begin(), edges.end(), edge);
+    edges.erase(to_delete);
+}
+
+template <class V, class E>
+bool Vertex<V,E>::operator ==(const Vertex<V,E> & value)
+{
+    return info == value.getInfo();
+}
+
+template <class V, class E>
+std::ostream & operator <<(std::ostream & os, const Vertex<V,E> & vertex)
+{
+    os << "Vertex: " << vertex.info << std::endl;
+
+    for (auto  e : vertex.edges) {
+        os << *e << std::endl;
+    }
+
+    return os;
+}
+
+#endif /* Vertex_hpp */
